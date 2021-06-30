ecdff8208a137e6b2451d812f06d5648c1d9fd23
JoseManuel <josemcastilloc@gmail.com>
2021-06-30 12:15:46 -0300

    adminFinal

---
+++ blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name= "Nombre")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name="categoria"
+        verbose_name="categorias"
+        ordering = ['-created']
+
+def __str__(self):
+    return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name= "Titulo")
+    content = models.TextField(verbose_name="contenido")
+    published = models.DateTimeField(verbose_name="Fecha de publicacion",default=now)
+    image = models.ImageField(verbose_name="Imagen", upload_to="blog",null=True, blank=True)
+    author = models.ForeignKey(User,verbose_name="Autor",on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category,verbose_name="categorias")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name="entrada"
+        verbose_name="entradas"
+        ordering = ['-created']
+
+def __str__(self):
+    return self.title
--- services/models.py
+++ services/models.py
@@ -3,16 +3,17 @@ from django.db.models.base import ModelState
 
 # Create your models here.
 class Service(models.Model):
-    title = models.CharField(max_length=200, verbose_name= "Titulo")
-    subtitle =models.CharField(max_length=200, verbose_name="Servicio")
+    title = models.CharField(max_length=200, verbose_name= "servicio")
+    subtitle =models.CharField(max_length=200, verbose_name="Servicios")
     content = models.TextField()
     image = models.ImageField(upload_to="services")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name="Titulo"
-        verbose_name="Servicio"
+        verbose_name="servicio"
+        verbose_name_plural="Servicios"
+        ordering = ['-created']
 
 def __str__(self):
     return self.title
